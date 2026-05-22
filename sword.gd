8da91ab30d8010595d300a1834f462d874e84f8d
Kohfi <dannylis953@gmail.com>
2026-05-22 23:45:23 +0800

    added directional attack

--- sword.gd
+++ sword.gd
@@ -10,12 +10,19 @@ var facing_right = true
 
 func attack_light():
 
+	# attack cooldown
 	if not can_attack:
 		return
 
 	can_attack = false
 
-	# FLIP every attack
+	# ROTATE TOWARD CURSOR
+	look_at(get_global_mouse_position())
+
+	# sprite correction
+	rotation -= deg_to_rad(90)
+
+	# optional alternating flip
 	facing_right = !facing_right
 	anim.flip_h = facing_right
 
